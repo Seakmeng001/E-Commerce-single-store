d6bf68b8f90ed62446aa4140f79e9190b17cc5b9
Anonymousdesu <chanwattana.som@gmail.com>
2024-02-08 22:38:05 +0700

    Fonts and with images with some page(login, promote, reg, shop)

---
+++ web/src/services/fetchData.js
@@ -0,0 +1,30 @@
+
+import axios from "axios";
+
+async function fetchData(method, url, data) {
+  try {
+    const token = localStorage.getItem("token");
+
+    const headers = {
+      Authorization: `Bearer ${token}`,
+      "Content-Type": "application/json",
+      Accept: "application/json",
+    };
+    const response = await axios({
+      method: method,
+      url: url,
+      data: data,
+      headers: headers,
+    });
+    return response.data;
+  } catch (error) {
+    if (error.response && error.response.status === 401) {
+      window.location.href = "/login";
+      return;
+    }
+    console.error("Error fetching data:", error);
+    throw error;
+  }
+}
+
+export default fetchData;
